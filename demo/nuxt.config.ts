4b7493f32c6d6d8569e2da14978db28dca8ddd37
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-04-11 10:07:47 +0200

    chore: update dev dependencies

--- demo/nuxt.config.ts
+++ demo/nuxt.config.ts
@@ -1,23 +1,22 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: "static",
-  head: {
-    link: [
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossorigin: true,
-      },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500display=swap",
-      },
-    ],
-  },
   css: ["@/assets/css/main.css"],
   app: {
     baseURL: "/postcss-fluid/",
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap",
+        },
+      ],
+    },
   },
   postcss: {
     plugins: {

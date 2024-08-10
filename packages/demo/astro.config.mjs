a72f37c14b6f98533895f0a01124cfffa6abae86
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-08-10 19:39:36 +0200

    fix github deployment

--- packages/demo/astro.config.mjs
+++ packages/demo/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lehoczky.github.io",
+  base: "postcss-fluid",
   integrations: [
     tailwind({
       applyBaseStyles: false,
